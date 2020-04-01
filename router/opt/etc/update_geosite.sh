e468ffa23e21f477fb1e90d588fc7cd710e7211d
Billy.Zheng <vil963@gmail.com>
2020-04-01 13:56:51 +0800

    Fix update_geosite.sh bug.
    Former-commit-id: fcfca6234af33458533f6a95157b65ca1b4df881

--- router/opt/etc/update_geosite.sh
+++ router/opt/etc/update_geosite.sh
@@ -6,11 +6,13 @@ tag=$(curl https://github.com/v2ray/domain-list-community/releases/latest |sed '
 
 curl -L https://github.com/v2ray/domain-list-community/releases/download/$tag/dlc.dat -o /opt/sbin/geosite.dat.new
 
+cd /opt/sbin
+
 if [ $? == 0 ]; then
     if [ "$(ls -l geosite.dat.new |awk '{print $5}')" -gt 700000 ]; then
-        rm -f /opt/sbin/geosite.dat.old
-        mv /opt/sbin/geosite.dat /opt/sbin/geosite.dat.old &&
-            mv /opt/sbin/geosite.dat.new /opt/sbin/geosite.dat &&
+        rm -f geosite.dat.old
+        mv geosite.dat geosite.dat.old &&
+            mv geosite.dat.new geosite.dat &&
             /opt/etc/init.d/S22v2ray restart
     fi
 else
