0690f9521476f9eed77bfd315178404d08df536f
Billy.Zheng <vil963@gmail.com>
2021-04-20 13:39:23 +0800

    Fix update_geosite.sh script.
    Former-commit-id: 635c1cf75886d71aaf630cd90cbbf015ac66b14b

--- router/opt/etc/update_geosite.sh
+++ router/opt/etc/update_geosite.sh
@@ -2,14 +2,14 @@
 
 # Update geosite.dat.
 
-tag=$(curl https://github.com/v2ray/domain-list-community/releases/latest |sed 's#.*href="\(.*\)".*#\1#g'|sed 's#.*/\([0-9]*\)#\1#g')
+tag=$(curl https://github.com/v2fly/domain-list-community/releases/latest |sed 's#.*href="\(.*\)".*#\1#g'|sed 's#.*/\([0-9]*\)#\1#g')
 
-curl -L https://github.com/v2ray/domain-list-community/releases/download/$tag/dlc.dat -o /opt/sbin/geosite.dat.new
-
-cd /opt/sbin
+curl -L https://github.com/v2fly/domain-list-community/releases/download/$tag/dlc.dat -o /opt/sbin/geosite.dat.new
 
 if [ $? == 0 ]; then
-    if [ "$(ls -l geosite.dat.new |awk '{print $5}')" -gt 700000 ]; then
+    cd /opt/sbin
+
+    if [ "$(ls -l geosite.dat.new |awk '{print $5}')" -gt 1000000 ]; then
         rm -f geosite.dat.old
         mv geosite.dat geosite.dat.old &&
             mv geosite.dat.new geosite.dat &&
