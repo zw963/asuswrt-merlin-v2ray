9c07a0b282aeef3330ff03c2d5b26485ba453ff1
Billy.Zheng <vil963@gmail.com>
2021-10-25 19:46:50 +0800

    Refactor

--- router/opt/etc/apply_iptables_rule.sh
+++ router/opt/etc/apply_iptables_rule.sh
@@ -33,54 +33,81 @@ if [ -z "$v2ray_server_ip" ]; then
     exit
 fi
 
-# 这里的 IP 不要随便改动或减少，尤其不要随便改动 127.0.0.1/8 为 127.0.0.1/32, 可能引起回环！
-LOCAL_IPS="
-0.0.0.0/8
-10.0.0.0/8
-127.0.0.0/8
-255.255.255.255
-$v2ray_server_ip
-"
-
 function apply_redirect_rule () {
     echo -n ' Applying redirect rule ...'
     iptables -t nat -N V2RAY_TCP # 代理局域网 TCP 流量
-    for local_ip in $LOCAL_IPS; do
-        iptables -t nat -A V2RAY_TCP -d $local_ip -j RETURN
-    done
+
+    # step 1: 所有针对本地地址、局域网地址、VPS 服务器地址的流量直连
+    iptables -t nat -A V2RAY_TCP -d 127.0.0.0/8 -j RETURN
+    iptables -t nat -A V2RAY_TCP -d $v2ray_server_ip -j RETURN
     iptables -t nat -A V2RAY_TCP -d 192.168.0.0/16 -j RETURN
-    # 如果是 V2Ray 标记过、并再次发出的流量(通过 streamSettings.sockopt.mark: 255 设置),
-    # 全部走直连，不这样做就成了死循环了。
+
+    # step 4: 从 V2Ray 发出的流量，再次经过时 netfilter 时，如果是 V2Ray 标记过
+    # 为 255 的流量，全部走直连.
     iptables -t nat -A V2RAY_TCP -p tcp -j RETURN -m mark --mark 0xff
+
+    # step 2: 所有剩下的流量会转发给 V2Ray 本地监听的端口.
     # REDIRECT其实是 DNAT 的一种特殊形式，
     # 特殊在其把数据包的目标 IP 改成了 127.0.0.1，端口改成了--to-ports 参数指定的本地端口，
     # 这样本机的透明代理程序就能处理这个包，应用能通过内核的状态信息拿到被改写之前的目标 IP 和端口号
     iptables -t nat -A V2RAY_TCP -p tcp -j REDIRECT --to-ports $local_v2ray_port
 
-    # apply rule
+    # step 3: V2Ray 内部处理，并将 outbounds 的流量全部通过 streamSettings.sockopt.mark: 255, set mark 255.
+
+    # 应用到 PREROUTING 关卡的事情：
+    # 1. 针对目标地址为本地地址、VPS 地址的数据包，直接返回
+    # 2. 剩下的目标地址为远程地址的数据包，全部转发到 local v2ray port
+
+    # 针对外部来的流量，应用 V2RAY_TCP 策略
     iptables -t nat -A PREROUTING -p tcp -j V2RAY_TCP
+
+    # 将 V2RAY_TCP 这个 rule-chain, 附加到 OUTPUT 这个网关的 `nat 占位符' 的最后面.
     iptables -t nat -A OUTPUT -p tcp -j V2RAY_TCP
 }
 
 function apply_tproxy_rule () {
     echo -n ' Applying TProxy rule ...'
-    # 使用一个特殊的路由，将数据包指向本地
+    # 由于使用了mangle表，所以数据包的原始和目的地址都是不会被修改的。
+
+    # 定义了一个叫做 V2RAY_UDP 的 empty chain.
+    iptables -t mangle -N V2RAY_UDP
+
+    # step 1: 所有针对本地地址、VPS 服务器地址的流量直连
+    iptables -t mangle -A V2RAY_UDP -d 127.0.0.0/8 -j RETURN
+    iptables -t mangle -A V2RAY_UDP -d $v2ray_server_ip -j RETURN
+
+    # step 2: 但是针对局域网地址，tcp 总是流量直连，目标地址是 53 的 udp 流量(局域网的DNS流量)，
+    # 则继续向前走.
+    iptables -t mangle -A V2RAY_UDP -d 192.168.0.0/16 -p tcp -j RETURN
+    iptables -t mangle -A V2RAY_UDP -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
+
+    # step 5: 从 V2Ray 发出的流量，再次经过时 netfilter 时，如果是 V2Ray 标记过
+    # 为 255 的流量，全部走直连.
+    iptables -t mangle -A V2RAY_UDP -j RETURN -m mark --mark 0xff
+
+    # More details, see https://www.kernel.org/doc/Documentation/networking/tproxy.txt
+
+    # 下面两行代码，将使用 --tproxy-mark 0x01/0x01 标记过的 udp/tcp 数据包路由到本机回环接口
+    # 简介实现了类似于 redirect 的功能，而且同时对 tcp/udp 生效.
     ip rule add fwmark 1 table 100
     ip route add local default dev lo table 100
 
-    # 由于使用了mangle表，所以数据包的原始和目的地址都是不会被修改的。
-    iptables -t mangle -N V2RAY_UDP
+    # step 3: 这个不会解释，反正知道和上面的 ip rule/route 一起，可以针对 tcp/udp 实现类似于 redirect 的功能。
+    # 同时需要在 V2Ray 的入栈的地方加
+    # "streamSettings": {
+    #     "sockopt": {
+    #         "tproxy": "tproxy"
+    #     }
+    # }
+    # 来确保 V2Ray 可以识别这种流量。
+    iptables -t mangle -A V2RAY_UDP -p udp -j TPROXY --tproxy-mark 1 --on-port $local_v2ray_port
+    iptables -t mangle -A V2RAY_UDP -p tcp -j TPROXY --tproxy-mark 1 --on-port $local_v2ray_port
 
-    for local_ip in $LOCAL_IPS; do
-        iptables -t mangle -A V2RAY_UDP -d $local_ip -j RETURN
-    done
 
-    iptables -t mangle -A V2RAY_UDP -d 192.168.0.0/16 -p tcp -j RETURN
-    # 本地局域网内，除了发至 53 端口的流量(会被 tproxy 标记)，其余全部直连.
-    iptables -t mangle -A V2RAY_UDP -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
+    # step 4: V2Ray 内部处理，outbounds 的地方也设定为 255.
 
-    iptables -t mangle -A V2RAY_UDP -p udp -j TPROXY --on-port $local_v2ray_port --tproxy-mark 1
-    iptables -t mangle -A V2RAY_UDP -p tcp -j TPROXY --on-port $local_v2ray_port --tproxy-mark 1
+
+    # 将 V2RAY_UDP 这个 rule-chain, 附加到 PREROUTING 这个网关的 `mangle 占位符' 的最后面.
     iptables -t mangle -A PREROUTING -j V2RAY_UDP
 }
 
@@ -90,16 +117,28 @@ function apply_gateway_rule () {
 
     iptables -t mangle -N V2RAY_MASK # 代理网关本机
 
-    # 192.168.0.0/16 必须全部返回，否则会造成 V2ray 中 DNS 策略中使用 localhost 时，形成死循环。
+    # iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p tcp -j RETURN
+    # iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
+
     iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -j RETURN
 
     # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面V2Ray 配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
     iptables -t mangle -A V2RAY_MASK -j RETURN -m mark --mark 0xff
 
+    # 在 OUTPUT 链打标记会使相应的包重路由到 PREROUTING 链上，
+    # 在已经配置好了 PREROUTING 相关的透明代理的情况下，OUTPUT 链也可以透明代理了，
     iptables -t mangle -A V2RAY_MASK -p udp -j MARK --set-mark 1   # 给 UDP 打标记,重路由
     iptables -t mangle -A V2RAY_MASK -p tcp -j MARK --set-mark 1   # 给 TCP 打标记，重路由
 
     iptables -t mangle -A OUTPUT -j V2RAY_MASK # 应用规则
+}
+
+function apply_socket_rule () {
+    # 新建 DIVERT 规则，避免已有连接的包二次通过 TPROXY，理论上有一定的性能提升
+    iptables -t mangle -N DIVERT
+    iptables -t mangle -A DIVERT -j MARK --set-mark 1
+    iptables -t mangle -A DIVERT -j ACCEPT
+    iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
 }
 
 if [ -e /opt/etc/use_redirect_proxy ]; then
@@ -110,6 +149,7 @@ else
         # 下面的 rule 使得路由器内访问 google 可以工作。
         # 似乎在 fakedns 模式下不工作。
         apply_gateway_rule
+        # apply_socket_rule
     else
         apply_redirect_rule
     fi
