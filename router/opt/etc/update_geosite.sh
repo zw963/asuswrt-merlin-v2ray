39996a745f3c3b4901e76fdaedd5fef0d3eeba07
Billy.Zheng <vil963@gmail.com>
2023-11-11 21:16:50 +0800

    Fix update_geosite.sh
    Former-commit-id: 917c2fa1b3f2b48fefdcb67f0ae84db1cc46d9fa

--- router/opt/etc/update_geosite.sh
+++ router/opt/etc/update_geosite.sh
@@ -1,31 +1,75 @@
 #!/bin/sh
 
-# Update geosite.dat.
+# Script for update geosite.dat, geoip-only-cn-private.dat.
 
-tag=$(curl https://github.com/v2fly/domain-list-community/releases/latest |sed 's#.*href="\(.*\)".*#\1#g'|sed 's#.*/\([0-9]*\)#\1#g')
+ROOT=${0%/*}
 
-curl -L https://github.com/v2fly/domain-list-community/releases/download/$tag/dlc.dat -o /opt/sbin/geosite.dat.new
+function github_latest_release () {
+    set -ue
+
+    project_name=$1
+
+    tag=$(wget -O - https://github.com/$project_name/releases/latest |egrep -o "$project_name/releases/tag/[^\"]*\" data-view-component" |cut -d'"' -f1 |rev|cut -d'/' -f1 |rev)
+
+    echo "$tag"
+}
+
+sitedata=geosite.dat
+ipdata=geoip-only-cn-private.dat
+
+geoip_tag=$(github_latest_release v2fly/geoip)
+
+geoip_url=https://github.com/v2fly/geoip/releases/download/${geoip_tag}/$ipdata
+wget -c $geoip_url -O $ROOT/${ipdata}.new
+
+
+geosite_tag=$(github_latest_release v2fly/domain-list-community)
+geosite_url=https://github.com/v2fly/domain-list-community/releases/download/${geosite_tag}/dlc.dat
+wget -c $geosite_url -O $ROOT/${sitedata}.new
 
 if [ $? == 0 ]; then
-    cd /opt/sbin
+    if  [ -e /opt/etc ]; then
+        # 假设是路由器环境
+        cd /opt/sbin
 
-    if [ -e /opt/etc/init.d/S22v2ray ]; then
-        /opt/etc/init.d/S22v2ray stop
+        if [ -e /opt/etc/init.d/S22v2ray ]; then
+            /opt/etc/init.d/S22v2ray stop
+        else
+            systemctl stop v2ray
+        fi
+
+        if [ "$(ls -l ${sitedata}.new |awk '{print $5}')" -gt 1500000 ]; then
+            cp $sitedata ${sitedata}.old
+            cp ${sitedata}.new $sitedata
+            rm -f ${sitedata}.new
+        fi
+
+        if [ "$(ls -l ${ipdata}.new |awk '{print $5}')" -gt 130000 ]; then
+            cp $ipdata ${ipdata}.old
+            cp ${ipdata}.new $ipdata
+            rm -f ${ipdata}.new
+        fi
+
+        if [ -e /opt/etc/init.d/S22v2ray ]; then
+            /opt/etc/init.d/S22v2ray start
+        else
+            systemctl start v2ray
+        fi
     else
-        systemctl stop v2ray
-    fi
+        # 假设是本机部署
+        if [ "$(ls -l ${sitedata}.new |awk '{print $5}')" -gt 1500000 ]; then
+            cp $sitedata ${sitedata}.old
+            cp ${sitedata}.new $sitedata
+            rm -f ${sitedata}.new
+        fi
 
-    rm -f geosite.dat.old
-    cp geosite.dat geosite.dat.old
+        if [ "$(ls -l ${ipdata}.new |awk '{print $5}')" -gt 130000 ]; then
+            cp $ipdata ${ipdata}.old
+            cp ${ipdata}.new $ipdata
+            rm -f ${ipdata}.new
+        fi
 
-    if [ "$(ls -l geosite.dat.new |awk '{print $5}')" -gt 1000000 ]; then
-         cp geosite.dat.new geosite.dat
-    fi
-
-    if [ -e /opt/etc/init.d/S22v2ray ]; then
-        /opt/etc/init.d/S22v2ray start
-    else
-        systemctl start v2ray
+        echo 'restart proxy to take effect.'
     fi
 else
     echo 'download failed.'
