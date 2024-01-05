9a2c76c538a58021c8c1ece8387348e61b755059
Billy.Zheng <vil963@gmail.com>
2024-01-05 22:58:29 +0800

    Check curl verion when check google use socks5

--- router/opt/etc/check_google_use_socks5.sh
+++ router/opt/etc/check_google_use_socks5.sh
@@ -14,9 +14,32 @@ else
     sleep=1
 fi
 
-if [ "$user_asuswrt" == "true" ]; then
-    echo 'Current curl verison not support socks5 proxy'
+curl_version=$(curl -V |head -n1 |awk '{print $2}')
+
+function version_less_than () {
+    local currentver="$1"
+    local requiredver="$2"
+    local sort
+
+    if sort -V --version &>/dev/null; then
+        sort='sort -V'
+    else
+        # sort -V 是 GNU 扩展，这里兼容性原因，选择 sort -n，但可能不准确
+        sort='sort -n'
+    fi
+
+    if [ "$(printf '%s\n' "$requiredver" "$currentver" | $sort | head -n1)" == "$requiredver" ]; then
+        # greater than or equal
+        false
+    else
+        # less than
+        true
+    fi
+}
+
+if version_less_than $curl_version 7.60.0; then
+    echo "Current curl verison $curl_version not support socks5 proxy, exit ..."
     exit 1
-else
-    curl -so /dev/null -w "Check ${1-google.com} use socks5://127.0.0.1:1080, %{http_code}\n" ${1-google.com} -x socks5://127.0.0.1:1080
 fi
+
+curl -so /dev/null -w "Check ${1-google.com} use socks5://127.0.0.1:1080, %{http_code}\n" ${1-google.com} -x socks5://127.0.0.1:1080
